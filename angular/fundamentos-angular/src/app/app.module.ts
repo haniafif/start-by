9146ffc397522709e9ff2a3e7c0b9c9708bcf405
Hani <haniaboultaif@gmail.com>
2022-12-21 16:00:47 -0300

    Feature aula 3 & 4

--- angular/fundamentos-angular/src/app/app.module.ts
+++ angular/fundamentos-angular/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { MultiplicaPorPipe } from './multiplica-por.pipe';
+import { PipeCostumizadoComponent } from './pipe-costumizado/pipe-costumizado.component';
+import { TwoWayDataBindComponent } from './two-way-data-bind/two-way-data-bind.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponenteComponent,
+    SegundoComponenteComponent,
+    MultiplicaPorPipe,
+    PipeCostumizadoComponent,
+    TwoWayDataBindComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
